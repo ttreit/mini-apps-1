2e4e0785f34de81d95844a871980bc6fa59bede7
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-29 17:50:04 -0700

    Add modules structure

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,4 @@
+import * as Test from './modules/board.js';
 
+Test.testFunction(1);
 
---
+++ challenge_1/modules/board.js
@@ -0,0 +1,6 @@
+let testFunction = function(input) {
+  console.log(input);
+  console.log(`Testing basic setup ${input} was passed in to this function`);
+};
+
+export { testFunction };
