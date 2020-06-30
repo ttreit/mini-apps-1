db7eb22cae5955a43100eb79ae06e12c704ea9c2
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-30 13:57:44 -0700

    Add on.click prototype

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,4 +1,9 @@
-import * as Test from './modules/mod1.js';
+document.getElementById('one').onclick = () => {
+  document.getElementById('one').innerHTML = 'X'
+  document.getElementById('one').style = 'Color: blue';
+}
 
-Test.testFunction(1);
-
+document.getElementById('two').onclick = () => {
+  document.getElementById('two').innerHTML = 'O'
+  document.getElementById('two').style = 'Color: red';
+}
