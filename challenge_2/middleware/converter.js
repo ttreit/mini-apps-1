3d1efd68b539229249da249c2a967fe9c8068f67
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-02 15:27:28 -0700

    Add parseData function - get column values (WIP)

--- challenge_2/middleware/converter.js
+++ challenge_2/middleware/converter.js
@@ -1,7 +1,93 @@
-let converter = function(req, res, next){
+const testData = {
+  "firstName": "Joshie",
+  "lastName": "Wyattson",
+  "county": "San Mateo",
+  "city": "San Mateo",
+  "role": "Broker",
+  "sales": 1000000,
+  "children": [
+  {
+    "test": 'TEST',  //remove this
+    "firstName": "Beth Jr.",
+    "lastName": "Johnson",
+    "county": "San Mateo",
+    "city": "Pacifica",
+    "role": "Manager",
+    "sales": 2900000,
+    "children": [
+      {
+        "firstName": "Smitty",
+        "lastName": "Won",
+        "county": "San Mateo",
+        "city": "Redwood City",
+        "role": "Sales Person",
+        "sales": 4800000,
+        "children": []
+      },
+      {
+        "firstName": "Allen",
+        "lastName": "Price",
+        "county": "San Mateo",
+        "city": "Burlingame",
+        "role": "Sales Person",
+        "sales": 2500000,
+        "children": []
+      }
+    ]
+  },
+  {
+    "firstName": "Beth",
+    "lastName": "Johnson",
+    "county": "San Francisco",
+    "city": "San Francisco",
+    "role": "Broker/Sales Person",
+    "sales": 7500000,
+    "children": []
+  }
+]
+}
 
-  next();
+let convertToCsv = function(req, res, next){
+  const csvRows = [];
+  //Get all keys and put in set
+  let parseData = function(dataObj) {
+    let keySet = new Set();
+    //do the work
+    for (key in dataObj) {
+      if (key !== 'children') {
+        keySet.add(key);
+      }
+    }
+    //check base case
+    //move toward base case
+    //recurse
+    console.log('****KEYSET ', keySet);
+  };
+  parseData(testData);  //TODO replace with correct argument
 };
 
+convertToCsv();
 
-module.exports = converter;
+
+
+module.exports = convertToCsv;
+
+
+
+  // keySet.add('firstName');
+  // keySet.add('lastName');
+  // console.log('*** keySet' ,keySet);
+  //const row1 = Object.keys(testData);
+
+  //create array to hold strings
+  //hookup test data (DEV ONLY)
+  //get keys for first line of csv
+  //NOTE: Child objects may have different properties - deal with this at the end if time simple solution first
+      //add commas between each
+      //add to array[0] (write to file maybe?);
+    //get values for all other lines
+    //add to array[1 ff]
+    //construct string to return
+      //array item 1 - arraylength -1 (fifo)
+      //each item on it's own line
+  //next();  TODO uncomment after testing done
