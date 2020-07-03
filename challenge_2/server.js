7db36547081cc44c5b58bdf0dc714cc320f34c21
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-02 23:29:16 -0700

    Add / route with index.html

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,7 +1,16 @@
-const express = require('express')
+const express = require('express');
+const http = require('http');
+const path = require('path');
 
 const app = express();
 const port = 3500;
+
+app.get('/', (req, res) => {
+  let options = {
+    root: path.join(__dirname, 'public')
+  }
+  res.sendFile('./index.html', options);
+});
 
 app.listen(port, () => console.log(`Server up and running on port ${port}`));
 
