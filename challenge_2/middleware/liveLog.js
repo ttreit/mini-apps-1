6a15a81eea163a4b6970d459ee4566366072dbb4
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-03 15:40:19 -0700

    Create logging middleware for troubleshooting

---
+++ challenge_2/middleware/liveLog.js
@@ -0,0 +1,7 @@
+let liveLogger = function(req, res, next) {
+  console.log('Incoming Request:');
+  console.log('Method :', req.method, ' --> URL: ', req.url);
+  next();
+};
+
+module.exports = liveLogger;
