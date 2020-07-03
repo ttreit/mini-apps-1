524329d9447f36940e1fc0da0b7a764c45eacb7e
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-02 23:38:21 -0700

    Update port to allow for process.env variable

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const path = require('path');
 
 const app = express();
-const port = 3500;
+const port = process.env.port || 3500;
 
 app.get('/', (req, res) => {
   let options = {
