4aae844309ac4dfffae1f7740b367a4a56362e86
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-04 00:15:00 -0700

    Add body-parser, urlencoded, and post route

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const bodyparser = require('body-parser');
 const liveLog = require('./middleware/liveLog');
+
 
 const app = express();
 const port = process.env.port || 3500;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(liveLog);
 
 app.get('/', (req, res) => {
@@ -13,6 +16,13 @@ app.get('/', (req, res) => {
     root: path.join(__dirname, 'public')
   }
   res.sendFile('./index.html', options);
+});
+
+app.post('/', (req, res) => {
+  let options = {
+    root: path.join(__dirname, 'public')
+  }
+  res.status(200).sendFile('./index.html', options);
 });
 
 app.get('/styles/main.css', (req, res) => {
@@ -23,9 +33,6 @@ app.get('/styles/main.css', (req, res) => {
 });
 
 
-app.post('/', (req, res) => {
-
-});
 
 app.listen(port, () => console.log(`Server up and running on port ${port}`));
 
