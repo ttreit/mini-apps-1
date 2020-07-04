4ca2debe850cf886bb6dc474c948bcb0b069d86d
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-04 00:10:19 -0700

    Add test for what is coming in the POST body

--- challenge_2/middleware/liveLog.js
+++ challenge_2/middleware/liveLog.js
@@ -1,6 +1,11 @@
 let liveLogger = function(req, res, next) {
+
   console.log('Incoming Request:');
   console.log('Method :', req.method, ' --> URL: ', req.url);
+  let test = req.body;
+  test = JSON.stringify(test);
+  console.log(test);
+
   next();
 };
 
