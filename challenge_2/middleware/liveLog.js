dc20ad6e1df6451ff1cf490ee4956647e8883e5c
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-04 21:04:48 -0700

    Refactor to log incoming requests method and path only

--- challenge_2/middleware/liveLog.js
+++ challenge_2/middleware/liveLog.js
@@ -2,11 +2,10 @@ let liveLogger = function(req, res, next) {
 
   console.log('Incoming Request:');
   console.log('Method :', req.method, ' --> URL: ', req.url);
-  let test = req.body;
-  test = JSON.stringify(test);
-  console.log(test);
 
   next();
 };
 
 module.exports = liveLogger;
+
+
