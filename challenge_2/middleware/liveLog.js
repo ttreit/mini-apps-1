99becac6ecd44f63b3617ac02453f0463b23afd7
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-05 12:16:15 -0700

    Revert to normal after using for testing

--- challenge_2/middleware/liveLog.js
+++ challenge_2/middleware/liveLog.js
@@ -2,7 +2,6 @@ let liveLogger = function(req, res, next) {
 
   console.log('Incoming Request:');
   console.log('Method :', req.method, ' --> URL: ', req.url);
-  console.log('Body ', req.body);
 
   next();
 };
