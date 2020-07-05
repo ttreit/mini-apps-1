374774c6d9091e91444a4e15e8c5018769167cc1
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-04 21:03:08 -0700

    Add modules for handling incoming JSON

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
+const qs = require('querystring');
 const liveLog = require('./middleware/liveLog');
 
 
 const app = express();
 const port = process.env.port || 3500;
 
+//app.use(bodyparser.json);
+//app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(liveLog);
+
 
 app.get('/', (req, res) => {
   let options = {
@@ -18,12 +22,14 @@ app.get('/', (req, res) => {
   res.sendFile('./index.html', options);
 });
 
+
 app.post('/', (req, res) => {
   let options = {
     root: path.join(__dirname, 'public')
   }
   res.status(200).sendFile('./index.html', options);
 });
+
 
 app.get('/styles/main.css', (req, res) => {
   let options = {
