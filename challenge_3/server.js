c23d6f780291bccc75039519eaa39967e1be1a38
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-06 20:41:30 -0700

    Create express server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 3300;
 
+
+
+
+
+
+app.listen(port, () => console.log(`Server up and listening on ${port}` ));
 
