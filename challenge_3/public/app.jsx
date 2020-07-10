6b4e5642b3b1ba259fdda5f9adbcc539a8728f5e
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-10 16:51:35 -0700

    Add basic form

--- challenge_3/public/app.jsx
+++ challenge_3/public/app.jsx
@@ -5,6 +5,7 @@ function MainContainer() {
   return (
   <div>
     <CompanyName companyname='Stuff Mart' />
+    <DataForm />
     <MainButton mainbuttonname='test' />
   </div>
   );
@@ -22,9 +23,104 @@ function MainButton(props) {
     console.log('The button was clicked!');
   }
 
-  return <button onClick={handleClick}>
+  return (
+  <button onClick={handleClick}>
     {props.mainbuttonname}
   </button>
+  );
+}
+
+//DataForm
+function DataForm() {
+  return (
+  <form action ='/submit' method='get' className='dataform'>
+    <div className='dataform'>
+      <label>
+        Name:
+        <input type='text' name='name' id='name'/>
+      </label>
+    </div>
+    <div className='dataform'>
+      <label>
+        Email:
+        <input type='text' email='email' id='email'/>
+      </label>
+    </div>
+    <div className='dataform'>
+      <label>
+        Password:
+        <input type='text' password='password' id='password'/>
+      </label>
+    </div>
+    <div className='dataform'>
+      <label>
+        Address1:
+        <input type='text' address1='address1' id='address1'/>
+      </label>
+    </div>
+    <div className='dataform'>
+      <label>
+        Address2:
+        <input type='text' address2='address2' id='address2'/>
+      </label>
+    </div>
+    <div className='dataform'>
+      <label>
+        City:
+        <input type='text' city='city' id='city' />
+      </label>
+    </div>
+    <div className='dataform'>
+      <label>
+        State:
+        <input type='text' state='state' id='state' />
+      </label>
+    </div>
+    <div className='dataform'>
+      <label>
+        Zipcode:
+        <input type='text' zipcode='zipcode' id='zipcode'/>
+      </label>
+    </div>
+    <div className='dataform'>
+      <label>
+        Phone:
+        <input type='text' phone='phone' id='phone'/>
+      </label>
+    </div>
+    <div className='dataform'>
+      <label>
+        Credit Card #:
+        <input type='text' ccnumber='ccnumber' id='ccnumber'/>
+      </label>
+    </div>
+    <div className='dataform'>
+      <label>
+        Expiration:
+        <input type='text' expirydate='expirydate' id='expirydate'/>
+      </label>
+    </div>
+    <div className='dataform'>
+      <label>
+        CVV:
+        <input type='text' cvv='cvv' id='cvv'/>
+      </label>
+    </div>
+    <div className='dataform'>
+    <label>
+      Billing Zipcode:
+      <input type='text' billingzipcode='billingzipcode' id='billingzipcode'/>
+    </label>
+    </div>
+    <div className='dataform'>
+      <input type='submit' value='Submit' />
+    </div>
+  </form>
+
+
+
+
+  );
 }
 
 //Add to Dom
