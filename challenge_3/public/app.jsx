d7ac0814035cb341d54363c5f7492457c48e8dea
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-10 15:35:29 -0700

    Add basic button component and clickhandler

--- challenge_3/public/app.jsx
+++ challenge_3/public/app.jsx
@@ -1,18 +1,35 @@
 //create components
 
 //MainContainer
-const MainContainer = <CompanyName companyname='Stuff Mart' />;
+function MainContainer() {
+  return (
+  <div>
+    <CompanyName companyname='Stuff Mart' />
+    <MainButton mainbuttonname='test' />
+  </div>
+  );
+}
+// button relies on state
 
 //CompanyName
 function CompanyName(props) {
   return <h1>Welcome to {props.companyname}!</h1>
 }
 
+//Button
+function MainButton(props) {
+  function handleClick(e) {
+    console.log('The button was clicked!');
+  }
 
+  return <button onClick={handleClick}>
+    {props.mainbuttonname}
+  </button>
+}
 
 //Add to Dom
 
 ReactDOM.render(
-  MainContainer,
+  <MainContainer />,
   document.getElementById('root')
 );
