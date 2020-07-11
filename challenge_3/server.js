66fb713607f06082d7481bf35d47d917aa966ad6
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-10 21:17:27 -0700

    Post help desk commit - server not serving static files

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,11 +1,26 @@
+//Require
 const express = require('express');
-const app = express();
+const path = require('path');
+
+//Server Setup
 const port = process.env.port || 3300;
+const app = express();
+
+//app.use(express.static('../../compiled/challenge_3/public'))
+//app.use(express.static('public'))
+//app.use(express.static('testfolder'))
+
+//Routes
+app.get('/', function(req, res){
+  const options = {
+    root: path.join(__dirname, 'public')
+  }
+  res.sendFile('./index.html', options);
+
+})
 
 
 
-
-
-
+//Invoke Server
 app.listen(port, () => console.log(`Server up and listening on ${port}` ));
 
